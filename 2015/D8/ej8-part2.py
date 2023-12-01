1a6ad0db9e6296426591122d273028b6f9ccf119
javipzv <90279135+javipzv@users.noreply.github.com>
2023-12-01 21:16:49 +0100

    added ej8

--- 2015/D8/ej8-part2.py
+++ 2015/D8/ej8-part2.py
@@ -7,19 +7,23 @@ for string in input:
     len_new_string = 0
     continuations = 0
 
-    for i in range(len(string)-1):
+    for i in range(len(string)):
         if continuations > 0:
             continuations -= 1
             continue
-        if (string[i] + string[i+1]) == "\\x":
+        # INITIAL ESCAPE
+        if i == 0:
+            len_new_string += 2
+        # END ESCAPE
+        elif i == (len(string) - 1):
+            len_new_string += 2
+        # SPECIAL CASES
+        elif (string[i] + string[i+1]) == "\\x":
             len_new_string += 1
-            continuations = 3
         elif (string[i] + string[i+1]) == "\\\\" or (string[i] + string[i+1]) == "\\\"":
-            len_new_string += 1
+            len_new_string += 2
             continuations = 1
-        else:
-            len_new_string += 1
     
-    total += (code_characters - (len_new_string - 1))
+    total += len_new_string
 
 print(total)
